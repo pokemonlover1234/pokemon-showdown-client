48a5e7ecbcf3220ea3a01d79428286df01318f0a
Aidan Fischer <aidanfischer@gmail.com>
2026-01-25 10:45:49 -0600

    lets see if this works!

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -230,16 +230,9 @@ export const Dex = new class implements ModdedDex {
 
 	pokeballs: string[] | null = null;
 
-	resourcePrefix = (() => {
-		let prefix = '';
-		if (window.document?.location?.protocol !== 'http:') prefix = 'https:';
-		return `${prefix}//${window.Config ? Config.routes.client : 'play.pokemonshowdown.com'}/`;
-	})();
+	resourcePrefix = 'https://raw.githubusercontent.com/pokemonlover1234/ionite-showdown-sprites/main/';
 
-	fxPrefix = (() => {
-		const protocol = (window.document?.location?.protocol !== 'http:') ? 'https:' : '';
-		return `${protocol}//${window.Config ? Config.routes.client : 'play.pokemonshowdown.com'}/fx/`;
-	})();
+	fxPrefix = 'https://raw.githubusercontent.com/pokemonlover1234/ionite-showdown-sprites/main/fx/';
 
 	loadedSpriteData = { xy: 1, bw: 0 };
 	moddedDexes: { [mod: string]: ModdedDex } = {};
